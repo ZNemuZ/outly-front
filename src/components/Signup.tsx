7db251ba4bf600534536f43b815c0e3b5d9c3a6a
= <168827531+ZNemuZ@users.noreply.github.com>
2024-12-15 21:56:32 +0900

    Mutationを追加

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,10 +1,34 @@
+import { FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useMutateAuth } from '../hooks/useMutateAuth'
 export const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [uname, setUname] = useState('')
+  const [pw, setPw] = useState('')
+  const { signupMutation, loginMutation } = useMutateAuth()
+
+  const submitSignupHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    await signupMutation
+      .mutateAsync({
+        email: email,
+        userName: uname,
+        password: pw,
+      })
+      .then(() =>
+        loginMutation.mutate({
+          email: email,
+          password: pw,
+        })
+      )
+  }
+
   return (
     <div className="bg-[#202123] min-h-screen flex justify-center items-center">
       <div className="bg-[#2b2d31] p-8 rounded-lg  shadow-[0px_0px_15px_rgba(255,255,255,0.2)] text-center max-w-sm w-full">
         <h1 className="text-3xl font-bold text-gray-300 mb-6">Sign Up</h1>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={submitSignupHandler}>
           <div className="flex items-center bg-[#202123] p-3 rounded-full shadow-inner">
             <span className="text-gray-400 text-xl mr-3">
               <i className="fas fa-user"></i>
