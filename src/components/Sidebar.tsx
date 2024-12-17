a31902566252071e7a1397570444dd318261e82f
= <168827531+ZNemuZ@users.noreply.github.com>
2024-12-17 20:49:50 +0900

    Sidebarレイアウト調整

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,10 +7,10 @@ import SidebarOption from './SidebarOption'
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#1E1E1E] min-h-screen w-[200px] flex flex-col justify-start items-center px-4">
+    <div className="bg-[#1E1E1E] min-h-screen w-[200px] flex flex-col justify-start items-center px-4 shadow-[0px_0px_8px_rgba(255,255,255,0.3)] pr-10">
       <div className="w-full flex flex-col space-y-4 mt-8">
         {/* User*/}
-        <AccountCircleOutlinedIcon className="text-gray-300 h-5 w-5 s-4" />
+        <AccountCircleOutlinedIcon className="text-gray-300 h-5 w-5" />
         {/* Home*/}
         <SidebarOption text="Home" Icon={HomeIcon} />
         {/* Search*/}
