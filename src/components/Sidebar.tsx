9ab0682d0b4a9476c76d8b169300926b4345767e
= <168827531+ZNemuZ@users.noreply.github.com>
2025-01-09 18:40:54 +0900

    サイドバーのクリック対応

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,8 +4,14 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox'
 import SettingsIcon from '@mui/icons-material/Settings'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 import SidebarOption from './SidebarOption'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
+  const navigate = useNavigate()
+
+  const onClick = () => {
+    navigate('/post')
+  }
   return (
     <div className="bg-[#1E1E1E] min-h-screen w-[200px] flex flex-col justify-start items-center px-4 shadow-[0px_0px_8px_rgba(255,255,255,0.3)] pr-10 fixed top-0 left-0 h-full">
       <div className="w-full flex flex-col space-y-4 mt-8">
@@ -13,11 +19,11 @@ const Sidebar = () => {
         <AccountCircleOutlinedIcon className="text-gray-300 h-5 w-5" />/}
 
         {/* Home*/}
-        <SidebarOption text="Home" Icon={HomeIcon} />
+        <SidebarOption text="Home" Icon={HomeIcon} onClick={onClick} />
         {/* Search
         <SidebarOption text="Search" Icon={SavedSearchIcon} />*/}
         {/* Profile*/}
-        <SidebarOption text="Profile" Icon={AccountBoxIcon} />
+        {/* <SidebarOption text="Profile" Icon={AccountBoxIcon} /> */}
         {/* Settings
         <SidebarOption text="Settings" Icon={SettingsIcon} />*/}
       </div>
--- src/components/SidebarOption.tsx
+++ src/components/SidebarOption.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type SidebarOptionProps = {
   text: string // 必須: 表示するテキスト (string型)
   Icon: React.ElementType // 必須: アイコン (Reactコンポーネント)
+  onClick?: () => void //
 }
 const SidebarOption: React.FC<SidebarOptionProps> = ({ text, Icon }) => {
   return (
