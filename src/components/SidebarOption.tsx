c55f9427c86ceb03bec6a7b43af79b68aa6dd8c2
= <168827531+ZNemuZ@users.noreply.github.com>
2025-01-10 03:55:26 +0900

    SidebarOptionにonClickを追加

--- src/components/SidebarOption.tsx
+++ src/components/SidebarOption.tsx
@@ -5,9 +5,16 @@ type SidebarOptionProps = {
   Icon: React.ElementType // 必須: アイコン (Reactコンポーネント)
   onClick?: () => void //
 }
-const SidebarOption: React.FC<SidebarOptionProps> = ({ text, Icon }) => {
+const SidebarOption: React.FC<SidebarOptionProps> = ({
+  text,
+  Icon,
+  onClick,
+}) => {
   return (
-    <div className="flex items-center space-x-3 p-3 rounded-lg cursor-pointer hover:bg-gray-700 transition duration-200 w-full">
+    <div
+      className="flex items-center space-x-3 p-3 rounded-lg cursor-pointer hover:bg-gray-700 transition duration-200 w-full"
+      onClick={onClick}
+    >
       <Icon className="text-gray-300 h-5 w-5" />
       <h2 className="text-gray-300 text-sm font-medium">{text}</h2>
     </div>
